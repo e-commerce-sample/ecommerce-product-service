139f3460c0a00a1d6045160d950e559c671928f0
davenkin <davenkin@126.com>
2019-07-31 10:51:28 +0800

    update common lib version to use publish transaction

--- src/main/java/com/ecommerce/product/ProductRabbitmqConfig.java
+++ src/main/java/com/ecommerce/product/ProductRabbitmqConfig.java
@@ -1,7 +1,9 @@
 package com.ecommerce.product;
 
 import com.ecommerce.common.event.EcommerceRabbitProperties;
+import com.google.common.collect.ImmutableMap;
 import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +18,14 @@ public class ProductRabbitmqConfig {
     public ProductRabbitmqConfig(EcommerceRabbitProperties properties) {
         this.properties = properties;
     }
+
+
+    //Inventory服务的"发送方Exchange"，通常不应该在消费方配置发送方的Exchange，这里只是作demo用
+    @Bean
+    public TopicExchange inventoryPublishExchange() {
+        return new TopicExchange("inventory-publish-x", true, false, ImmutableMap.of("alternate-exchange", "inventory-publish-dlx"));
+    }
+
 
     //接收inventory上下文的InventoryChangedEvent事件，用于实时更新product对应的库存量
     @Bean
